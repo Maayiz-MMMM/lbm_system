0f12f34ac4719f9e306763d5ac146fcec7d31a6e
maayis <sahalumsahala@gmail.com>
2026-01-17 02:13:14 +0530

    icon

--- view/auth/login.php
+++ view/auth/login.php
@@ -20,6 +20,8 @@ $rememberEmail = $_COOKIE['remember_email'] ?? '';
     <meta charset="UTF-8">
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="icon" href="<?= asset('assets/images/Puttalam-Smart-Library.png')  ?>">
+
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet">
